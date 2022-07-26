0bebb9491a976d6dbc4223d1e788b3bd5746dc87
Sergei Maznoi <s.maznoi888@gmail.com>
2022-07-26 16:20:49 +0300

    add project

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import $ from "jquery";
+
+// slick slider
+
+$('.multiple-items').slick({
+  infinite: true,
+  autoplay: true,
+  autoplaySpeed: 3000,
+  slidesToShow: 2,
+  slidesToScroll: 2
+});
+
---
+++ src/js/modal.js
@@ -0,0 +1,20 @@
+(() => {
+  const btnOpen = document.querySelectorAll("[data-modal-open]")
+  .forEach(el => el.addEventListener("click", openModal));
+
+  const refs = {
+      closeModalBtn: document.querySelector("[data-modal-close]"),
+      modal: document.querySelector("[data-modal]"),
+    };
+  
+  refs.closeModalBtn.addEventListener("click", closeModal);
+  
+    function openModal() {
+      refs.modal.classList.remove("is-hidden");
+    }
+
+    function closeModal() {
+      refs.modal.classList.add("is-hidden");
+    }
+  })();
+  
