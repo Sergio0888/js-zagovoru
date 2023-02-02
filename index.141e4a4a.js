e093d69451ffcdd5dd0c34c827a483a1ef5e7f9f
Sergio0888 <99532626+Sergio0888@users.noreply.github.com>
2023-02-02 09:53:06 +0000

    Deploying to gh-pages from @ Sergio0888/js-zagovoru@a4c1bffcae68b0af53db1710d66f5437f84267f9 🚀

--- index.141e4a4a.js
+++
@@ -1,2 +0,0 @@
-const n=document.querySelector(".btn");console.log(n),onClickBtn=n=>{console.log(n)},n.addEventListener("click",onClickBtn);
-//# sourceMappingURL=index.141e4a4a.js.map
--- index.8ff2ffd8.js
+++
@@ -1,2 +0,0 @@
-!function(){var n=document.querySelector(".btn");console.log(n),onClickBtn=function(n){console.log(n)},n.addEventListener("click",onClickBtn)}();
-//# sourceMappingURL=index.8ff2ffd8.js.map
